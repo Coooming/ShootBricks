e412b692bc1df3aab258f020e1cd1ed9bd098999
Coooming <coming.coming@qq.com>
2022-02-13 17:16:07 +0800

    upload project files and v1.0

---
+++ Assets/Scripts/Rename.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rename : MonoBehaviour
+{
+    void Reset() // 在编辑器中点击Reset时调用
+    {
+        int len = transform.childCount;
+        for (int i = 0; i < len; i++)
+        {
+            transform.GetChild(i).name = "Cube" + (i + 1).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 20;
+	
+	void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 判断是否按下了鼠标，0为鼠标左键
+        {
+            GameObject bulletObject = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody bulletRigidbody = bulletObject.GetComponent<Rigidbody>();
+            bulletRigidbody.velocity = transform.forward * speed;
+        }
+    }
+}
